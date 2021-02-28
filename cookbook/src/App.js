5badbde89ace4b63d80df095d417c34ef77b4256
Jakeas <JakeASierra@gmail.com>
2021-02-28 13:04:45 -0500

    initial commit - working on basics of sign-up page

--- cookbook/src/App.js
+++ cookbook/src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import yup from 'yup'
+
+//initial states
+
+const initialFormValues = {
+  username: '',
+  password: '', 
+}
+const initialFormErrors = {
+  username: '',
+  password: '',
+}
+const initialData = []
+const initialDisabled = true
+
+//main function
 
 function App() {
+
+//slices of state set at initial values
+
+const [ data, setData ] = useState(initialData)
+const [ formValues, setFormValues ] = useState(initialFormValues)
+const [ formErrors, setFormErrors ] = useState(initialFormErrors)
+const [ disabled, setDisabled ] = useState(initialDisabled)
+
+const getRecipes = () => {
+  axios
+    .get('https://tt67recipes.herokuapp.com/api/users/:id/recipes')
+    .then((res) => {
+      console.log(res)
+    })
+
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
