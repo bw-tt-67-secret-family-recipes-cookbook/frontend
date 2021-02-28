5538a59d26d6fc75adc76d11bc690cfc4ef2dafa
Jakeas <JakeASierra@gmail.com>
2021-02-28 18:29:47 -0500

    worked on filling out app and formSchema

--- cookbook/src/App.js
+++ cookbook/src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import yup from 'yup'
+import * as yup from 'yup'
+import formSchema from './formSchema'
+import Register from './Register';
 
 //initial states
 
 const initialFormValues = {
   username: '',
-  password: '', 
+  password: '',
+  passwordConfirm: '',
+  accept: false 
 }
 const initialFormErrors = {
   username: '',
   password: '',
+  passwordConfirm: '',
+  accept: false
 }
 const initialRecipes = []
 const initialDisabled = true
@@ -52,39 +58,57 @@ const postRecipes = newRecipe => {
     setFormValues(initialFormValues)
 }
 
-// const inputChange = (name, value) => {
-//   yup
-//     .reach(formSchema, name)
-//     .validate(value)
-//     .then (()=>{
-//       setFormErrors({...setFormErrors.formErrors, [name]:''})
-//     })
-//     .catch((err)=>{
-//       setFormErrors({...formErrors, [name]: err.errors[0]})
-//     })
-//     setFormValues({
-//       ...formValues, 
-//       [name]: value
-//     })
-// }
+const inputChange = (name, value) => {
+  yup
+    .reach(formSchema, name)
+    .validate(value)
+    .then (()=>{
+      setFormErrors({...setFormErrors.formErrors, [name]:''})
+    })
+    .catch((err)=>{
+      setFormErrors({...formErrors, [name]: err.errors[0]})
+    })
+    setFormValues({
+      ...formValues, 
+      [name]: value
+    })
+}
 
 const formSubmit = () => {
   const newRecipe = {
     username: formValues.username.trim(),
     password: formValues.password.trim(),
+    passwordConfirm: formValues.passwordConfirm.trim(),
+    terms: ['accept'].filter(term => formValues[term])
   }
   postRecipes(newRecipe)
 }
 
-useEffect(()=>{
+useEffect(() => {
   getRecipes()
 }, [])
 
+useEffect(() => {
+formSchema.isValid(formValues).then(valid => setDisabled(!valid))
+}, [formValues])
+
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <h1>Title of Page</h1>
+      <Register 
+      values={formValues}
+      errors={formErrors}
+      disabled={disabled}
+      submit={formSubmit}
+      change={inputChange}
+      />
+      { 
+        // recipes.map(recipe => {
+        // return(
+        // <Recipe key={recipe.id} details={recipe}
+        // )
+        // })
+      }
     </div>
   );
 }
