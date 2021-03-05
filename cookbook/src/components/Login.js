0c3455812bedc85f3ba1b36b59b4c40dfd2c9902
Cheyenne Floyd <cheyennesfloyd@gmail.com>
2021-03-04 19:26:13 -0500

    styled login and signup

--- cookbook/src/components/Login.js
+++ cookbook/src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
 
 function Login(props){
     const {values, submit, change, disabled, errors} = props
@@ -24,41 +25,117 @@ function Login(props){
         history.push('/')
       }
 
+    const Wrapper = styled.div`
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: white;
+        background: linear-gradient(to left, #aa6639, white);
+    `
+
+    const Form = styled.form` 
+        background: url(https://www.tastymatters.com/wp-content/uploads/2017/06/cooking-with-copper-cookware-health-4.jpg) no-repeat center center fixed;
+        color: white;
+        text-shadow: 1px 3px 3px brown;
+        font-weight: bold; 
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 350px;
+        width: 600px;
+    `
+    const Middle = styled.div``
+
+    const Title = styled.h1``
+
+    const SubTitle = styled.h2``
+
+    const Meat = styled.div`
+        display: flex;
+        flex-direction: row;
+    `
+    const Label = styled.div``
+    
+    const Input = styled.input`
+        background: beige;
+    `
+    const ButtonWrapper = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 50%;
+        margin-left: 25%;
+    `
+    const Login = styled.button`
+        margin: 2rem;
+        padding: 1rem 2rem;
+        background: #aa6639;
+        color: white;
+        border: none;
+        transition: .25s ease-in-out;
+
+        &:hover {
+            cursor: pointer;
+            background: sienna;
+            color: #aa6639;
+        }
+    `
+    const Home = styled.button`
+        margin: 2rem;
+        padding: 1rem 2rem;
+        background: #aa6639;
+        color: white;
+        border: none;
+        transition: .25s ease-in-out;
+
+        &:hover {
+            cursor: pointer;
+            background: sienna;
+            color: #aa6639;
+        }
+    `
+
     return(
-        <form onSubmit={onSubmit}>
-        <div>
-            <h1>Login</h1>
-            <h2>Welcome Back!</h2>
+        <Wrapper>
+        <Form onSubmit={onSubmit}>
+            <Middle>
+            <Title>Login</Title>
+            <SubTitle>Welcome Back!</SubTitle>
 
             <div>{errors.username}</div>
             <div>{errors.password}</div>
+        <Meat>
 
-            <label>Username
-                <input
+            <Label>Username
+                <Input
                     value={values.username}
                     onChange={onChange}
                     name='username'
                     type='text'
                 />
-            </label>
+            </Label>
             
-            <label>Password
-                <input
+            <Label>Password
+                <Input
                     value={values.password}
                     onChange={onChange}
                     name='password'
                     type='text'
                 />
-            </label>
-
-            <button onClick=
-            {routeToRecipe}>Login</button>
-            <button onClick=
-            {routeToHome}>Home</button>
-
-        </div>    
-        </form>
-        
+            </Label>
+            </Meat>
+            <ButtonWrapper>
+            <Login onClick=
+            {routeToRecipe}>Login</Login>
+            <Home onClick=
+            {routeToHome}>Home</Home>
+            </ButtonWrapper>
+            
+            </Middle>    
+        </Form>
+        </Wrapper>
     )
 
 }
--- cookbook/src/components/SignupForm.js
+++ cookbook/src/components/SignupForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
 
 function SignupForm(props) {
     const {values, submit, change, disabled, errors} = props
@@ -15,44 +16,130 @@ function SignupForm(props) {
     const history = useHistory()
 
     const routeToLogin = () => {
-      console.log(history);
-      history.push('/login')
+        console.log(history);
+        history.push('/login')
     }
     const routeToHome = () => {
-      console.log(history);
-      history.push('/')
+        console.log(history);
+        history.push('/')
     }
+    const Wrapper = styled.div`
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: linear-gradient(to left, #aa6639, white);
+    `
+    const Form = styled.form`
+        background: url(https://videohive.img.customer.envatousercontent.com/files/2e6a7460-1df5-4651-9ac2-622670174f98/inline_image_preview.jpg?auto=compress%2Cformat&fit=crop&crop=top&max-h=8000&max-w=590&s=a53bf67b7db29ada57d832a1f308ba3c) no-repeat center center fixed;
+        color: white;
+        text-shadow: 1px 3px 3px brown;
+        font-weight: bold; 
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 600px;
+        width: 600px;
+    `
+    const Middle = styled.div``
+
+    const Title = styled.h1``
+
+    const Meat = styled.div`
+        display: flex;
+        flex-direction: row;
+    `
+    const Label = styled.div``
+    
+    const Input = styled.input`
+        background: tan;
+    `
+
+    const ButtonWrapper = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 50%;
+        margin-left: 25%;
+    `
+
+    const Signup = styled.button`
+        margin: 2rem;
+        padding: 1rem 2rem;
+        background: #aa6639;
+        color: white;
+        font-weight: bold; 
+        border: none;
+        transition: .25s ease-in-out;
+
+        &:hover {
+            cursor: pointer;
+            background: sienna;
+            color: #aa6639;
+        }
+    `
+    const Home = styled.button`
+        margin: 2rem;
+        padding: 1rem 2rem;
+        background: #aa6639;
+        color: white;
+        font-weight: bold; 
+        border: none;
+        transition: .25s ease-in-out;
+
+        &:hover {
+            cursor: pointer;
+            background: sienna;
+            color: #aa6639;
+        }
+    `
+
+   
+
+
     return(
-        <form onSubmit={onSubmit}>
-            <div>
-            <h1>Join us!</h1>
+        <Wrapper>
+            
+        <Form onSubmit={onSubmit}>
+        <Middle>
+            
+            <Title>Join us!</Title>
 
             <div>{errors.username}</div>
             <div>{errors.password}</div>
-
-            <label>Username
-                <input
+<Meat>
+            <Label>Username
+                <Input
                     value={values.username}
                     onChange={onChange}
                     name='username'
                     type='text'
                 />
-            </label>
+            </Label>
 
-            <label>Password
-                <input
+            <Label>Password
+                <Input
                     value={values.password}
                     onChange={onChange}
                     name='password'
                     type='text'
                 />
-            </label>
+            </Label>
+        </Meat>
+        <ButtonWrapper>
+            <Signup onClick={routeToLogin}>SignUp</Signup>
+            
+            <Home onClick={routeToHome}>Home
+            </Home>
+        </ButtonWrapper>
 
-            <button onClick={routeToLogin}>SignUp</button>
-            <button onClick={routeToHome}>Home</button>
-
-            </div>
-        </form>)
+            
+            </Middle>
+        </Form>
+        
+        </Wrapper>
+    )
 }
 
 export default SignupForm
